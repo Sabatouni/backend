5cc08ffb43080a2ddd22e88e1fa1e502ef124a2a
Sabatouni <arefhimid@gmail.com>
2026-05-04 22:22:59 +0800

    update new

--- src/main/java/com/swahili/pos/controller/ExpenseController.java
+++ src/main/java/com/swahili/pos/controller/ExpenseController.java
@@ -1,4 +1,4 @@
-package com.yourapp.controller;
+package com.swahili.pos.controller;
 
 import java.util.List;
 
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.swahili.pos.model.Expense;
+import com.swahili.pos.service.ExpenseService;
 
 @RestController
 @RequestMapping("/api/expenses")
